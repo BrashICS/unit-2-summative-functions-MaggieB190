783e34d0824ac05de41255829bada83ae690ee89
Maggie Boyd <maggie.boyd@stu.ocsb.ca>
2024-10-24 21:00:28 -0400

    Before final comit, finished

--- main.js
+++ main.js
@@ -11,21 +11,33 @@
 /*** Event Listeners ***/
 
 // volume button is the id in html to decide when to listen, click is what to listen for, volume is the function done
-document.getElementById("rect_prism_volume_button").addEventListener("click",rect_prism_volume);
-document.getElementById("rect_prism_area_button").addEventListener("click",rect_prism_area);
 
-document.getElementById("triangle_prism_volume_button").addEventListener("click",triangle_prism_volume);
-document.getElementById("triangle_prism_area_button").addEventListener("click",triangle_prism_area);
+document.getElementById("cube_volume_button").addEventListener("click", cube_volume);
+document.getElementById("cube_area_button").addEventListener("click", cube_area);
+
+document.getElementById("rect_prism_volume_button").addEventListener("click", rect_prism_volume);
+document.getElementById("rect_prism_area_button").addEventListener("click", rect_prism_area);
+
+document.getElementById("triangle_prism_volume_button").addEventListener("click", triangle_prism_volume);
+document.getElementById("triangle_prism_area_button").addEventListener("click", triangle_prism_area);
+
+document.getElementById("pyramid_volume_button").addEventListener("click", pyramid_volume);
+document.getElementById("pyramid_area_button").addEventListener("click", pyramid_area);
 
 document.getElementById("sphere_area_button").addEventListener("click", sphere_area);
 document.getElementById("sphere_volume_button").addEventListener("click", sphere_volume);
+
+document.getElementById("cylinder_area_button").addEventListener("click", cylinder_area);
+document.getElementById("cylinder_volume_button").addEventListener("click", cylinder_volume);
 
 document.getElementById("user_slope_points_button").addEventListener("click", user_slope);
 document.getElementById("user_length_points_button").addEventListener("click", user_length);
 document.getElementById("user_midpoint_button").addEventListener("click", midpoint);
 
-document.getElementById("zeros").addEventListener("click", zeros);
-document.getElementById("vertex").addEventListener("click", vertex);
+document.getElementById("zeros_button").addEventListener("click", zeros);
+document.getElementById("vertex_button").addEventListener("click", vertex);
+document.getElementById("aos_button").addEventListener("click", aos);
+document.getElementById("y_intercept_button").addEventListener("click", y_intercept);
 
 /*** Functions ***/
 
@@ -62,6 +74,20 @@ function length(x1, y1, x2, y2) {
     return l_coordinates;
 }
 
+//Cube volume
+function cube_volume() {
+    let base_cube =  Number(document.getElementById("base_cube").value);
+    let cube_v = round_user(base_cube ** 3);
+    document.getElementById("cube_volume_answer").textContent = `The volume for the cube is: ${cube_v}`;
+}
+
+//cube area
+function cube_area() {
+    let base_cube = Number(document.getElementById("base_cube").value);
+    let cube_a = round_user(6 *(base_cube ** 2));
+    document.getElementById("cube_area_answer").textContent = `The surface area for the cube is: ${cube_a}`;
+}
+
 //Part 1.7
 function rect_prism_volume() {
     //have to tell where to get variables from and give it a label (let statement), then make it do the math, then output
@@ -70,7 +96,7 @@ function rect_prism_volume() {
     let width = Number(document.getElementById("width").value);
     let height = Number(document.getElementById("height").value);
     let rect_volume = round_user(length * width * height);
-    document.getElementById("rect_prism_volume_answer").textContent = `The volume for the rectangular prism is: ${rect_volume}.`;
+    document.getElementById("rect_prism_volume_answer").textContent = `The volume for the rectangular prism is: ${rect_volume}`;
 }
 
 //Part 1.8
@@ -79,16 +105,24 @@ function rect_prism_area() {
     let width = Number(document.getElementById("width").value);
     let height = Number(document.getElementById("height").value);
     let rect_area = round_user(2 *((length * width) + (length * height) + (height * width)));
-    document.getElementById("rect_prism_area_answer").textContent = `The surface area for the rectangular prism is: ${rect_area}.`;
+    document.getElementById("rect_prism_area_answer").textContent = `The surface area for the rectangular prism is: ${rect_area}`;
 }
 
-//triangular prsim volume
+//Square Pyramid volume
+function pyramid_volume() {
+    let base_pyramid = Number(document.getElementById("base_pyramid").value);
+    let height_pyramid = Number(document.getElementById("height_pyramid").value);
+    let pyramid_v = round_user(1 / 3 *(base_pyramid **2 *(height_pyramid)));
+    document.getElementById("pyramid_volume_answer").textContent = `The volume for the square-based pyramid is: ${pyramid_v}`;
+}
+
+//triangular prism volume
 function triangle_prism_volume() {
     let length_trig = Number(document.getElementById("length_trig").value);
     let base_trig = Number(document.getElementById("base_trig").value);
     let height_trig = Number(document.getElementById("height_trig").value);
-    let triangle_volume = round_user((length_trig * base_trig * height_trig) / 2);
-    document.getElementById("triangle_prism_volume_answer").textContent = `The volume for the triangular prism is: ${triangle_volume}.`;
+    let triangle_v = round_user((length_trig * base_trig * height_trig) / 2);
+    document.getElementById("triangle_prism_volume_answer").textContent = `The volume for the triangular prism is: ${triangle_v}`;
 }
 
 //triangular prism area
@@ -96,9 +130,32 @@ function triangle_prism_area() {
     let length_trig = Number(document.getElementById("length_trig").value);
     let base_trig = Number(document.getElementById("base_trig").value);
     let height_trig = Number(document.getElementById("height_trig").value);
-    let triangle_area = round_user();
-    //MUST MAKE FORUMLA^^^^^
-    document.getElementById("triangle_prism_area_answer").textContent = `The surface area of the triangular prism is: ${triangle_area}.`;
+    let triangle_a = round_user((base_trig * height_trig) + (base_trig * length_trig) + (length_trig * height_trig));
+    document.getElementById("triangle_prism_area_answer").textContent = `The surface area for the triangular prism is: ${triangle_a}`;
+}
+
+//Square Pyramid area
+function pyramid_area() {
+    let base_pyramid = Number(document.getElementById("base_pyramid").value);
+    let height_pyramid = Number(document.getElementById("height_pyramid").value);
+    let pyramid_a = round_user(2 *(height_pyramid * base_pyramid *(base_pyramid ** 2)));
+    document.getElementById("pyramid_area_answer").textContent = `The surface area for the square-based pyramid is: ${pyramid_a}`;
+}
+
+//cylinder volume
+function cylinder_volume() {
+    let radius_cylinder = Number(document.getElementById("radius_cylinder").value);
+    let height_cylinder = Number(document.getElementById("height_cylinder").value);
+    let cylinder_v = round_user(height_cylinder *(Math.PI *(radius_cylinder ** 2)));
+    document.getElementById("cylinder_volume_answer").textContent = `The volume of the cylinder is: ${cylinder_v}`;
+}
+
+//cylinder area
+function cylinder_area() {
+    let radius_cylinder = Number(document.getElementById("radius_cylinder").value);
+    let height_cylinder = Number(document.getElementById("height_cylinder").value);
+    let cylinder_a = round_user((2 *(Math.PI *(radius_cylinder * height_cylinder))) + (2 *(Math.PI *(radius_cylinder ** 2))));
+    document.getElementById("cylinder_area_answer").textContent = `The surface area of the cylinder is: ${cylinder_a}`;
 }
 
 //Part 1.9
@@ -107,14 +164,14 @@ function triangle_prism_area() {
 function sphere_volume() {
     let radius = Number(document.getElementById("radius").value);
     let sphere_v = Number(round_user(4 / 3 *(Math.PI *(radius ** 3))));
-    document.getElementById("sphere_volume_answer").textContent = `The volume of the sphere is: ${sphere_v}.`;
+    document.getElementById("sphere_volume_answer").textContent = `The volume of the sphere is: ${sphere_v}`;
 }
 
 //function sphere area
 function sphere_area() {
     let radius = Number(document.getElementById("radius").value); 
     let sphere_a = Number(round_user(4 * (Math.PI * (radius ** 2))));
-    document.getElementById("sphere_area_answer").textContent = `The area of the sphere is: ${sphere_a}.`;
+    document.getElementById("sphere_area_answer").textContent = `The area of the sphere is: ${sphere_a}`;
 }
 
 //1.11 
@@ -125,7 +182,7 @@ function user_slope() {
     let x2 = Number(document.getElementById("x2").value);
     let y2 = Number(document.getElementById("y2").value);
     let user_m = Number(round_user(slope(x1, y1, x2, y2)));
-    document.getElementById("user_slope_points_answer").textContent = `The slope of the two points is: ${user_m}.`;
+    document.getElementById("user_slope_points_answer").textContent = `The slope of the two points is: ${user_m}`;
 }
 
 function user_length() {
@@ -134,7 +191,7 @@ function user_length() {
     let x2 = Number(document.getElementById("x2").value);
     let y2 = Number(document.getElementById("y2").value);
     let user_length = Number(round_user(length(x1, y1, x2, y2)));
-    document.getElementById("user_length_points_answer").textContent = `The length between the two points is: ${user_length}.`;
+    document.getElementById("user_length_points_answer").textContent = `The length between the two points is: ${user_length}`;
 }
 
 function midpoint() {
@@ -144,7 +201,7 @@ function midpoint() {
     let y2 = Number(document.getElementById("y2").value);
     let user_mid_x = Number(round_user(average(x1, x2)));
     let user_mid_y = Number(round_user(average(y1, y2)));
-    document.getElementById("user_midpoint_answer").textContent = `The midpoint of the two points is: (${user_mid_x}, ${user_mid_y}).`; 
+    document.getElementById("user_midpoint_answer").textContent = `The midpoint of the two points is: (${user_mid_x}, ${user_mid_y})`; 
 }
 
 
@@ -155,8 +212,6 @@ function y_quad(a, b, c, x) {
 }
 
 // Determine the zeros of a quadratic using user-inputs for a, b, and c
-//when y=o then find x's
-
 function zeros() {
     let a = Number(document.getElementById("a").value);
     let b = Number(document.getElementById("b").value);
@@ -164,7 +219,7 @@ function zeros() {
     let x1 = round_user(((-b) +(Math.sqrt((b ** 2) - (4 * a * c))))/ (2 * a));
     let x2 = round_user(((-b) -(Math.sqrt((b ** 2) - (4 * a * c))))/ (2 * a));
     console.log(`(${x1}, 0), (${x2}, 0)`);
-    document.getElementById("quadratic_output_zeros").textContent = `The zeros of this parabola is: (${x1}, 0) and (${x2}, 0).`; 
+    document.getElementById("quadratic_output_zeros").textContent = `The zeros of this parabola are: (${x1}, 0) and (${x2}, 0)`; 
 }
 
 // Determine the vertex of a quadratic using user-inputs for a, b, and c
@@ -175,5 +230,19 @@ function vertex() {
     let x = Number(round_user((-b) / (2 * a)));
     let y = round_user(y_quad(a, b, c, x));
     console.log(`(${x}, ${y})`);
-    document.getElementById("quadratic_output_vertex").textContent = `The vertex of this parabola is: (${x}, ${y}).`; 
+    document.getElementById("quadratic_output_vertex").textContent = `The vertex of this parabola is: (${x}, ${y})`; 
+}
+
+//find axis of symmetry
+function aos() {
+    let a = Number(document.getElementById("a").value);
+    let b = Number(document.getElementById("b").value);
+    let x = Number(round_user((-b) / (2 * a)));
+    document.getElementById("quadratic_output_aos").textContent = `The axis of symmetry of this parabola is: ${x}`; 
+}
+
+//find y-intercept
+function y_intercept() {
+        let c = Number(document.getElementById("c").value);
+        document.getElementById("quadratic_output_y_intercept").textContent = ` The y-intercept of this parabola is: (0, ${c})`;
 }
